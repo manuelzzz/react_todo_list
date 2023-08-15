1e099016676165656daa7548e3794d6418c8f654
Manel <manuelss.23.bil@gmail.com>
2023-08-15 16:27:35 -0300

    Turn the task to complete or incomplete and delete

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,33 @@ import Todo from "./components/Todo";
 import { TodoType } from "./types/Todo.types";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
   const [error, setError] = useState({});
+
+  const handleCompleted = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
+  const handleDelete = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
-      .then(res => setTodos(res.slice(0, 10)))
+      .then(res => setTodos(res.slice(0, 15)))
       .catch(err => setError(err))
   }, []);
 
   return (
     <div className="App">
-      {todos.length > 0 ? todos.map((todo: TodoType) => <Todo todo={todo} />) : <Loader />}
+      {todos.length > 0 ? todos.map((todo: TodoType, index: number) =>
+        <Todo todo={todo} index={index} handleCompleted={handleCompleted} handleDelete={handleDelete} />)
+        : <Loader />}
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,12 +2,27 @@ import React from "react";
 import { TodoType } from "../types/Todo.types";
 
 interface Props {
-    todo: TodoType
+    todo: TodoType;
+    index: number;
+    handleCompleted: (id: number) => void;
+    handleDelete: (id: number) => void;
 }
 
-const Todo: React.FC<Props> = ({ todo }) => {
+const Todo: React.FC<Props> = ({ todo, index, handleCompleted, handleDelete }) => {
     return (
-        <>Todo ..</>
+        <div className="todo" style={{
+            textDecoration: todo.completed ? 'line-through' : ''
+        }}>
+            {todo.title}
+            <div>
+                <button onClick={() => handleCompleted(index)}>
+                    {todo.completed ? 'Incomplete' : 'Complete'}
+                </button>
+                <button onClick={() => handleDelete(index)}>
+                    Delete
+                </button>
+            </div>
+        </div>
     );
 }
 
